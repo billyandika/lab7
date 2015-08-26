4df58f02e1e6b9da7c8f858e9feb1a829b11e4b8
Billy Andika <bandika@ucsd.edu>
2015-08-26 15:08:42 +0700

    update

--- models.js
+++ models.js
@@ -5,9 +5,9 @@ var Mongoose = require('mongoose');
 var ProjectSchema = new Mongoose.Schema({
   // fields are defined here
     	"title": String,
-  	"date": String,
-  	"summary": String,
-  	"image": String
+  	"name": String,
+  	"id": String,
+  	"details": String
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
