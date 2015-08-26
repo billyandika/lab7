36527cef63289010f0a107657da8092f28feddbd
Billy Andika <bandika@ucsd.edu>
2015-08-26 22:27:17 +0700

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var handlebars = require('express3-handlebars');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
-var project = require('./routes/project');
+// var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -45,7 +45,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/project/:id', project.projectInfo);
+// app.get('/project/:id', project.projectInfo);
 
 // Example route
 // app.get('/users', user.list);
