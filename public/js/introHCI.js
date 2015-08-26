65d4f3360ab0755c687211ec918cf575ade9d95e
Billy Andika <bandika@ucsd.edu>
2015-08-26 14:58:26 +0700

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -37,41 +37,39 @@ function initializePage() {
 			// compose the HTML
 			var new_html =
 				'<div class="project-date">'+display_date+'</div>'+
-				'<div class="project-summary">'+project_json['summary']+'</div>'+
-				'<button class="project-delete btn btn-default" '+
-					'type="button">delete</button>';
+				'<div class="project-summary">'+project_json['summary']+'</div>';
 
 			// get the DIV to add content to
 			var details_div = $('#project' + idNumber + ' .details');
 			// add the content to the DIV
 			details_div.html(new_html);
 
-			details_div.find('.project-delete').click(function(e) {
-				$.post('/project/'+idNumber+'/delete', function() {
-					window.location.href = '/';
-				});
-			});
+			// details_div.find('.project-delete').click(function(e) {
+			// 	$.post('/project/'+idNumber+'/delete', function() {
+			// 		window.location.href = '/';
+			// 	});
+			// });
 		}
 
 		// issue the GET request
 		$.get(url_call, addProjectDetails);
 	});
 
-	$('#newProjectSubmitButton').click(function(e) {
-		console.log('clicked');
-		var title = $('#new-project-form #title').val();
-		var image_url = $('#new-project-form #image_url').val();
-		var date = $('#new-project-form #date').val();
-		var summary = $('#new-project-form #summary').val();
-		var json = {
-			'project_title': title,
-			'image_url': image_url,
-			'date':  date,
-			'summary': summary
-		};
-		$.post('/project/new', json, function() {
-			window.location.href = '/'; // reload the page
-		});
-	});
+	// $('#newProjectSubmitButton').click(function(e) {
+	// 	console.log('clicked');
+	// 	var title = $('#new-project-form #title').val();
+	// 	var image_url = $('#new-project-form #image_url').val();
+	// 	var date = $('#new-project-form #date').val();
+	// 	var summary = $('#new-project-form #summary').val();
+	// 	var json = {
+	// 		'project_title': title,
+	// 		'image_url': image_url,
+	// 		'date':  date,
+	// 		'summary': summary
+	// 	};
+	// 	$.post('/project/new', json, function() {
+	// 		window.location.href = '/'; // reload the page
+	// 	});
+	// });
 }
 
